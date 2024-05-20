cbd9d61c86d7a2831a265f0813dc36e6fc2492a7
NatkoCar <natko.car@gmail.com>
2024-05-20 11:16:08 +0200

    Pocetak

---
+++ CodeLingo.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void startScreen()
+{
+    cout << "Dobrodošli u CodeLingo!" << endl;
+    cout << "1. Igraj!" << endl;
+    cout << "2. Rekordi" << endl;
+    cout << "9. Izlaz" << endl;
+    cout << "Unesite svoj izbor: ";
+}
+
+int main()
+{
+    int izbor;
+    while (1)
+    {
+        startScreen();
+
+        if (izbor == 1) // Igraj!
+        {
+        }
+
+        if (izbor == 2) // Rekordi
+        {
+        }
+
+        if (izbor == 9)
+        {
+            cout << "Doviđenja!" << endl;
+            return 0;
+        }
+    }
+}
